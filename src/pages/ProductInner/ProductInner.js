b3e7ba0196d1295365599f548fb6aa1f80e150dd
dima0208 <mashenko0208@gmail.com>
2023-02-22 11:54:04 +0200

    fixed_bug_while_ProductInner_is_loading

--- src/pages/ProductInner/ProductInner.js
+++ src/pages/ProductInner/ProductInner.js
@@ -51,48 +51,54 @@ export function ProductInner() {
       {loading && <Preloader />}
       {isError && <ErrorMessage />}
       <div className="container">
-        <Link className="ProductInner__link" to="/products">
-          Go to dogs list
-        </Link>
-        <div className="ProductInner__content">
+        <div className="ProductInner__link-container">
           {productImageUrl && (
-            <div className="ProductInner__image-container">
+            <Link className="ProductInner__link" to="/products">
+              Go to dogs list
+            </Link>
+          )}
+        </div>
+        <div className="ProductInner__content">
+          <div className="ProductInner__image-container">
+            {productImageUrl && (
               <img
                 className="ProductInner__image"
                 src={productImageUrl}
                 alt="fine_dog"
               />
-            </div>
-          )}
-          {product && (
-            <div className="ProductInner__info-container">
-              <h2 className="ProductInner__title">{product.name}</h2>
-              <h5 className="ProductInner__subtitle">
-                <b>Bred for: </b>
-                {product.bred_for.toLowerCase()}
-              </h5>
-              <h5 className="ProductInner__subtitle">
-                <b>Country origin:</b>
-                {product.origin ? product.origin : 'unknown'}
-              </h5>
-              <h5 className="ProductInner__subtitle">
-                <b>Height:</b>
-                {` ${product.height.metric} cm (${product.height.imperial} inch)`}
-              </h5>
-              <h5 className="ProductInner__subtitle">
-                <b>Weight:</b>
-                {` ${product.weight.metric} kg (${product.weight.imperial} lb)`}
-              </h5>
-              <h5 className="ProductInner__subtitle">
-                <b>Life span: </b>
-                {product.life_span}
-              </h5>
-              <h5 className="ProductInner__subtitle">
-                <b>Temperament: </b>
-                {product.temperament.toLowerCase()}
-              </h5>
-            </div>
-          )}
+            )}
+          </div>
+          <div className="ProductInner__info-container">
+            {product && (
+              <>
+                <h2 className="ProductInner__title">{product.name}</h2>
+                <h5 className="ProductInner__subtitle">
+                  <b>Bred for: </b>
+                  {product.bred_for.toLowerCase()}
+                </h5>
+                <h5 className="ProductInner__subtitle">
+                  <b>Country origin:</b>
+                  {product.origin ? product.origin : 'unknown'}
+                </h5>
+                <h5 className="ProductInner__subtitle">
+                  <b>Height:</b>
+                  {` ${product.height.metric} cm (${product.height.imperial} inch)`}
+                </h5>
+                <h5 className="ProductInner__subtitle">
+                  <b>Weight:</b>
+                  {` ${product.weight.metric} kg (${product.weight.imperial} lb)`}
+                </h5>
+                <h5 className="ProductInner__subtitle">
+                  <b>Life span: </b>
+                  {product.life_span}
+                </h5>
+                <h5 className="ProductInner__subtitle">
+                  <b>Temperament: </b>
+                  {product.temperament.toLowerCase()}
+                </h5>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
